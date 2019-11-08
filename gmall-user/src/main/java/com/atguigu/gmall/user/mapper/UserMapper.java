23561f1eeb9c1bf2e19cbcd04e22361870199928
wanghongda <541588991@qq.com>
2019-11-08 17:08:46 +0800

    增加

--- gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UserMapper.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package com.atguigu.gmall.user.mapper;
 
-public interface UserMapper {
+import com.atguigu.gmall.user.bean.UmsMember;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface UserMapper extends Mapper<UmsMember> {
+    List<UmsMember> selectAllUser();
 }
--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,41 @@
 package com.atguigu.gmall.user.service.impl;
 
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
 import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+        return userMapper.selectAll();
+        //return userMapper.selectAllUser();
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        UmsMemberReceiveAddress u = new UmsMemberReceiveAddress();
+        u.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> select = umsMemberReceiveAddressMapper.select(u);
+
+        Example e = new Example(UmsMemberReceiveAddress.class);
+        e.createCriteria().andEqualTo("memberId",memberId);
+        umsMemberReceiveAddressMapper.selectByExample(e);
+
+        return select;
+    }
 }
